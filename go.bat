8c3d8a8c999c891159cc321478873c78a90976c4
d0k3 <tore.anon@gmail.com>
2015-11-08 16:21:25 +0100

    Added a check for hs.app before processing

--- go.bat
+++ go.bat
@@ -1,3 +1,3 @@
 @echo off
-for %%x in (input\*.cia) do call tools\process.bat %%x input\hs.app
+if exist input\hs.app (for %%x in (input\*.cia) do call tools\process.bat %%x input\hs.app) else (echo [!] hs.app not found, use Decrypt9 to dump it on your 3DS)
 pause
