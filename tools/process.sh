e3a0e203bee3e80daa43feca1de99aacab35064b
Herz <achwazer@gmail.com>
2016-03-18 10:25:46 -0600

    Add OS X support and clean output

--- tools/process.sh
+++ tools/process.sh
@@ -2,6 +2,17 @@
 
 clear
 
+#tools
+_3DSTOOL=./tools/3dstool-linux
+CTRTOOL=./tools/ctrtool-linux
+MERGEEXHEADER=./tools/MergeExHeader-linux
+if [ "$(uname -s)" == "Darwin" ]
+then
+_3DSTOOL=./tools/3dstool-osx
+CTRTOOL=./tools/ctrtool-osx
+MERGEEXHEADER=./tools/MergeExHeader-osx
+fi
+
 printf " --- UNIVERSAL INJECT GENERATOR V0.3 ---\n"
 printf "          --- LINUX EDITION --- \n\n\n"
 
@@ -11,45 +22,45 @@ rm -rf work_old
 rm -rf hs.*
 mkdir work
 cp $2 work/hs.app
-./tools/ctrtool-linux -x --contents work/ciacnt $1
+${CTRTOOL} -x --contents work/ciacnt $1 &>/dev/null
 mv work/ciacnt.0000.* work/inject.app
 
 printf "[+] EXTRACT HS AND INJECT APP\n"
-tools/3dstool-linux -x -f work/hs.app --header work/hs_hdr.bin --exh work/hs_exhdr.bin --plain work/hs_plain.bin --logo work/hs_logo.bin --exefs work/hs_exefs.bin --romfs work/hs_romfs.bin
-tools/3dstool-linux -x -f work/inject.app --exh work/inject_exhdr.bin --exefs work/inject_exefs.bin
-tools/3dstool-linux -x -f work/hs_exefs.bin --exefs-dir work/hs_exefs
-tools/3dstool-linux -x -f work/inject_exefs.bin --exefs-dir work/inject_exefs
+${_3DSTOOL} -x -f work/hs.app --header work/hs_hdr.bin --exh work/hs_exhdr.bin --plain work/hs_plain.bin --logo work/hs_logo.bin --exefs work/hs_exefs.bin --romfs work/hs_romfs.bin &>/dev/null
+${_3DSTOOL} -x -f work/inject.app --exh work/inject_exhdr.bin --exefs work/inject_exefs.bin &>/dev/null
+${_3DSTOOL} -x -f work/hs_exefs.bin --exefs-dir work/hs_exefs &>/dev/null
+${_3DSTOOL} -x -f work/inject_exefs.bin --exefs-dir work/inject_exefs &>/dev/null
 
 printf "[+] GENERATE NEW EXEFS\n"
 cp work/inject_exefs/code.bin work/hs_exefs/code.bin
-tools/3dstool-linux -c -z -t exefs -f work/hs_mod_exefs.bin --exefs-dir work/hs_exefs --header work/hs_exefs.bin
+${_3DSTOOL} -c -z -t exefs -f work/hs_mod_exefs.bin --exefs-dir work/hs_exefs --header work/hs_exefs.bin &>/dev/null
 cp work/inject_exefs/banner.bnr work/hs_exefs/banner.bnr
 cp work/inject_exefs/icon.icn work/hs_exefs/icon.icn
-tools/3dstool-linux -c -z -t exefs -f work/hs_mod_banner_exefs.bin --exefs-dir work/hs_exefs --header work/hs_exefs.bin
+${_3DSTOOL} -c -z -t exefs -f work/hs_mod_banner_exefs.bin --exefs-dir work/hs_exefs --header work/hs_exefs.bin &>/dev/null
 
 printf "[+] GENERATE NEW ROMFS\n"
 mkdir work/dummy_romfs
 cp tools/dummy.bin work/dummy_romfs/dummy.bin
-tools/3dstool-linux -c -t romfs -f work/dummy_romfs.bin --romfs-dir work/dummy_romfs
+${_3DSTOOL} -c -t romfs -f work/dummy_romfs.bin --romfs-dir work/dummy_romfs &>/dev/null
 
 printf "[+] MERGE EXHEADER\n"
-tools/MergeExHeader-linux work/inject_exhdr.bin work/hs_exhdr.bin work/merge_exhdr.bin
+${MERGEEXHEADER} work/inject_exhdr.bin work/hs_exhdr.bin work/merge_exhdr.bin &>/dev/null
 
 printf "[+] REBUILD HS INJECT APP\n"
 
 if [ -e work/hs_logo.bin ]
-then tools/3dstool-linux -c -t cxi -f ${1%.*}_inject_no_banner.app --header work/hs_hdr.bin --exh work/merge_exhdr.bin --plain work/hs_plain.bin --logo work/hs_logo.bin --exefs work/hs_mod_exefs.bin --romfs work/dummy_romfs.bin 
-else tools/3dstool-linux -c -t cxi -f ${1%.*}_inject_no_banner.app --header work/hs_hdr.bin --exh work/merge_exhdr.bin --plain work/hs_plain.bin --exefs work/hs_mod_exefs.bin --romfs work/dummy_romfs.bin
+then ${_3DSTOOL} -c -t cxi -f ${1%.*}_inject_no_banner.app --header work/hs_hdr.bin --exh work/merge_exhdr.bin --plain work/hs_plain.bin --logo work/hs_logo.bin --exefs work/hs_mod_exefs.bin --romfs work/dummy_romfs.bin &>/dev/null
+else ${_3DSTOOL} -c -t cxi -f ${1%.*}_inject_no_banner.app --header work/hs_hdr.bin --exh work/merge_exhdr.bin --plain work/hs_plain.bin --exefs work/hs_mod_exefs.bin --romfs work/dummy_romfs.bin &>/dev/null
 fi
 
 if [ -e work/hs_logo.bin ]
-then tools/3dstool-linux -c -t cxi -f ${1%.*}_inject_with_banner.app --header work/hs_hdr.bin --exh work/merge_exhdr.bin --plain work/hs_plain.bin --logo work/hs_logo.bin --exefs work/hs_mod_banner_exefs.bin --romfs work/dummy_romfs.bin
-else tools/3dstool-linux -c -t cxi -f ${1%.*}_inject_with_banner.app --header work/hs_hdr.bin --exh work/merge_exhdr.bin --plain work/hs_plain.bin --exefs work/hs_mod_banner_exefs.bin --romfs work/dummy_romfs.bin
+then ${_3DSTOOL} -c -t cxi -f ${1%.*}_inject_with_banner.app --header work/hs_hdr.bin --exh work/merge_exhdr.bin --plain work/hs_plain.bin --logo work/hs_logo.bin --exefs work/hs_mod_banner_exefs.bin --romfs work/dummy_romfs.bin &>/dev/null
+else ${_3DSTOOL} -c -t cxi -f ${1%.*}_inject_with_banner.app --header work/hs_hdr.bin --exh work/merge_exhdr.bin --plain work/hs_plain.bin --exefs work/hs_mod_banner_exefs.bin --romfs work/dummy_romfs.bin &>/dev/null
 fi
 
-for i in work/hs.app; do HS_ORIGINAL_SIZE=$(stat -c %s $i); done
-for i in ${1%.*}_inject_no_banner.app; do HS_INJECT_N_SIZE=$(stat -c %s $i); done
-for i in ${1%.*}_inject_with_banner.app; do HS_INJECT_B_SIZE=$(stat -c %s $i); done
+for i in work/hs.app; do HS_ORIGINAL_SIZE=$(ls -l $i | awk '{print $5}'); done
+for i in ${1%.*}_inject_no_banner.app; do HS_INJECT_N_SIZE=$(ls -l $i | awk '{print $5}'); done
+for i in ${1%.*}_inject_with_banner.app; do HS_INJECT_B_SIZE=$(ls -l $i | awk '{print $5}'); done
 
 mv ${1%.*}_inject_no_banner.app ./
 mv ${1%.*}_inject_with_banner.app ./
