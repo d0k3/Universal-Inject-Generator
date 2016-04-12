54b378f69155668af8fea755d616d96454a4525b
d0k3 <tore.anon@gmail.com>
2016-04-12 22:40:06 +0200

    Properly handle inject app romfs (Windows)

--- tools/process.bat
+++ tools/process.bat
@@ -1,6 +1,6 @@
 @echo off
 
-echo [!] --- UNIVERSAL INJECT GENERATOR V0.3 ---
+echo [!] --- UNIVERSAL INJECT GENERATOR v0.5 ---
 echo.
 echo [+] CLEAR WORKING DIRECTORY
 rd /s /q work
@@ -13,8 +13,8 @@ ren "work\ciacnt.0000.*" inject.app
 
 echo.
 echo [+] EXTRACT HS AND INJECT APP
-tools\3dstool -x -f work\hs.app --header work\hs_hdr.bin --exh work\hs_exhdr.bin --plain work\hs_plain.bin --logo work\hs_logo.bin --exefs work\hs_exefs.bin --romfs work\hs_romfs.bin
-tools\3dstool -x -f work\inject.app --exh work\inject_exhdr.bin --exefs work\inject_exefs.bin
+tools\3dstool -x -f work\hs.app --header work\hs_hdr.bin --exh work\hs_exhdr.bin --plain work\hs_plain.bin --logo work\hs_logo.bin --exefs work\hs_exefs.bin
+tools\3dstool -x -f work\inject.app --exh work\inject_exhdr.bin --exefs work\inject_exefs.bin --romfs work\inject_romfs.bin
 tools\3dstool -x -f work\hs_exefs.bin --exefs-dir work\hs_exefs
 tools\3dstool -x -f work\inject_exefs.bin --exefs-dir work\inject_exefs
 
@@ -31,6 +31,7 @@ echo [+] GENERATE NEW ROMFS
 md work\dummy_romfs
 copy /y /v tools\dummy.bin work\dummy_romfs\dummy.bin
 tools\3dstool -c -t romfs -f work\dummy_romfs.bin --romfs-dir work\dummy_romfs
+if not exist work\inject_romfs.bin ren work\dummy_romfs work\inject_romfs.bin
 
 echo.
 echo [+] MERGE EXHEADER
@@ -38,8 +39,9 @@ tools\MergeExHeader work\inject_exhdr.bin work\hs_exhdr.bin work\merge_exhdr.bin
 
 echo.
 echo [+] REBUILD HS INJECT APP
-if exist work\hs_logo.bin (tools\3dstool -c -t cxi -f "%~n1_inject_no_banner.app" --header work\hs_hdr.bin --exh work\merge_exhdr.bin --plain work\hs_plain.bin --logo work\hs_logo.bin --exefs work\hs_mod_exefs.bin --romfs work\dummy_romfs.bin) else (tools\3dstool -c -t cxi -f %~n1_inject_no_banner.app --header work\hs_hdr.bin --exh work\merge_exhdr.bin --plain work\hs_plain.bin --exefs work\hs_mod_exefs.bin --romfs work\dummy_romfs.bin)
-if exist work\hs_logo.bin (tools\3dstool -c -t cxi -f "%~n1_inject_with_banner.app" --header work\hs_hdr.bin --exh work\merge_exhdr.bin --plain work\hs_plain.bin --logo work\hs_logo.bin --exefs work\hs_mod_banner_exefs.bin --romfs work\dummy_romfs.bin) else (tools\3dstool -c -t cxi -f %~n1_inject_with_banner.app --header work\hs_hdr.bin --exh work\merge_exhdr.bin --plain work\hs_plain.bin --exefs work\hs_mod_banner_exefs.bin --romfs work\dummy_romfs.bin)
+if exist work\hs_logo.bin (tools\3dstool -c -t cxi -f %~n1_inject_no_banner.app --header work\hs_hdr.bin --exh work\merge_exhdr.bin --plain work\hs_plain.bin --logo work\hs_logo.bin --exefs work\hs_mod_exefs.bin --romfs work\inject_romfs.bin) else (tools\3dstool -c -t cxi -f %~n1_inject_no_banner.app --header work\hs_hdr.bin --exh work\merge_exhdr.bin --plain work\hs_plain.bin --exefs work\hs_mod_exefs.bin --romfs work\inject_romfs.bin)
+if exist work\hs_logo.bin (tools\3dstool -c -t cxi -f %~n1_inject_with_banner.app --header work\hs_hdr.bin --exh work\merge_exhdr.bin --plain work\hs_plain.bin --logo work\hs_logo.bin --exefs work\hs_mod_banner_exefs.bin --romfs work\inject_romfs.bin) else (tools\3dstool -c -t cxi -f %~n1_inject_with_banner.app --header work\hs_hdr.bin --exh work\merge_exhdr.bin --plain work\hs_plain.bin --exefs work\hs_mod_banner_exefs.bin --romfs work\inject_romfs.bin)
+
 for %%i in (work\hs.app) do set HS_ORIGINAL_SIZE=%%~zi
 for %%i in ("%~n1_inject_no_banner.app") do set HS_INJECT_N_SIZE=%%~zi
 for %%i in ("%~n1_inject_with_banner.app") do set HS_INJECT_B_SIZE=%%~zi
